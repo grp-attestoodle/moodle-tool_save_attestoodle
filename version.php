0db83d58f2ccd02ee049a28a71f129fb6ad6bee7
Leconte <marc.leconte@univ-lemans.fr>
2019-11-15 15:20:12 +0100

    Update version.php

--- version.php
+++ version.php
@@ -25,10 +25,10 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'tool_save_attestoodle';
-$plugin->release = '0.1.0';
-$plugin->version = 2019062102;
+$plugin->release = '0.1.1';
+$plugin->version = 2019111501;
 $plugin->requires = 2012112900;
-$plugin->maturity = MATURITY_ALPHA;
+$plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = [
     'tool_attestoodle' => 2019061101,
 ];
