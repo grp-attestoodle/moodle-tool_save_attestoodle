e8983583e5591d7a642705f93ee3a16c0351d75c
poleumdev <64774219+poleumdev@users.noreply.github.com>
2020-07-15 10:49:26 +0200

    Update version.php

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'tool_save_attestoodle';
-$plugin->release = '0.1.1';
-$plugin->version = 2019111501;
+$plugin->release = '0.1.2';
+$plugin->version = 2020071501;
 $plugin->requires = 2012112900;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = [
